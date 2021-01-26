c1cd0ff39bdf8c9fee549236cd376fc9b483d740
liuwentao <wentao.liu01@hand-china.com>
2021-01-26 23:27:49 +0800

    root

---
+++ .umirc.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  chunks: ['app'],
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  /**
+   * package.json 配置 "renderer"."sourceDirectory": null
+   * 配置 打包输出路径 ('dist/renderer') 代替渲染进程打包文件
+   */
+  outputPath: 'dist/renderer',
+  fastRefresh: {},
+  history: { type: 'hash' },
+  dynamicImport: {},
+  runtimePublicPath: true,
+  // 配置 './' 用于 直接electron main进程直接访问 index.html 
+  publicPath: './',
+});
